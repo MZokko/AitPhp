2e8c7d9c866bf13ff0bece65570dd76bd054b21d
Alex <akekiwy@gmail.com>
2018-12-13 06:03:13 +0000

    categories side nav

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <?php
 include('vendor/autoload.php');
 //include('autoloader.php');
+//generate product
 use aitsyd\Product;
 
 $product_class = new Product();
 $products = $product_class->getProducts();
+$page_title = 'shop Page';
+
+//generate categories
+use aitsyd\Categories;
+$category_class = new Categories();
+$categories = $category_class->getCategories();
+$active_category = $category_class->getActive();
+
 
 $loader = new Twig_Loader_Filesystem('templates');
 
 $twig = new Twig_Environment($loader, array(
-    'cache' => 'cache',));
+    //'cache' => 'cache',
+    ));
 
 $template = $twig ->load('home.twig');
-echo $template -> render(array('products' => $products));
+echo $template -> render(array(
+    'pages'=>$pages,
+    'products'=>$products,
+    'pageTitle'=>$page_title,
+    'currentPage'=>$currentPage,
+    'categories'=> $categories,
+    'activeCategory' => $active_category
+    ));
 
 ?>
