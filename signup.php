9e7dffdd5758b0750ebcb4e67ede37a0039de595
Alex <akekiwy@gmail.com>
2018-12-20 04:51:13 +0000

    adding validator class + sign in

---
+++ signup.php
@@ -0,0 +1,21 @@
+<?php
+
+include('vendor/autoload.php');
+
+include ('includes/navigation.inc.php');
+
+$loader = new Twig_Loader_Filesystem('templates');
+
+$twig = new Twig_Environment($loader, array(
+    //'cache' => 'cache',
+    ));
+    
+$template = $twig ->load('signup.twig');
+
+echo $template -> render(array(
+    'pages'=>$pages,
+    'pageTitle'=>$page_title,
+    'currentPage'=>$currentPage,
+    ));
+
+?>
